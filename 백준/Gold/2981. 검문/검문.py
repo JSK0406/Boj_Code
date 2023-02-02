c06a0039064c228514eba92186e99af24f245e3f
Jiseong Kim <122510664+JSK0406@users.noreply.github.com>
2023-02-02 23:53:43 +0900

    Update 검문.py

--- 백준/Gold/2981. 검문/검문.py
+++ 백준/Gold/2981. 검문/검문.py
@@ -6,11 +6,11 @@ N = int(sys.stdin.readline())
 nums = [int(sys.stdin.readline()) for _ in range(N)]
 nums.sort()
 
-dif = nums[-1] - nums[0]
+dif = nums[-1] - nums[0]  # 리스트 정렬 후 가장 큰 수와 가장 작은 수의 차이
 sqrt_dif = int(math.sqrt(dif)) + 1
-possible = set()
+possible = set()  # 중복을 막기위해 set사용
 
-for i in range(1, sqrt_dif):
+for i in range(1, sqrt_dif):  # 1은 답이 될 수 없지만 가장 큰 수가 답이 될 수도 있으므로 1부터
     if dif % i == 0:
         possible.add(i)
         possible.add(dif // i)
@@ -28,4 +28,4 @@ for div in possible:
         result.append(div)
 
 result.sort()
-print(*result[1:])
+print(*result[1:])  # 1도 답에 포함되어 있을 것이므로 1번째 인덱스부터 슬라이싱하여 출력
