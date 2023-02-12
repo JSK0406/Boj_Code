0bc27f442f9cd92d1a41261cd224d7ae9125f819
JiSeong Kim <122510664+JSK0406@users.noreply.github.com>
2023-02-12 17:28:53 +0900

    [Gold IV] Title: 문자열 폭발, Time: 476 ms, Memory: 71692 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -2,16 +2,15 @@ import sys
 
 stack = []
 
-str_input = list(sys.stdin.readline()[::].strip())
-find = list(sys.stdin.readline()[::].strip())
+str_input = list(sys.stdin.readline().strip())
+find = list(sys.stdin.readline().strip())
 last = find[-1]
 
 for i in str_input:
     stack.append(i)
     if i == last:
         if find == stack[len(stack)-len(find):len(stack)]:
-            for _ in range(len(find)):
-                stack.pop()
+            del stack[len(stack)-len(find):len(stack)]
 
 if stack:
     print(*stack, sep='')
