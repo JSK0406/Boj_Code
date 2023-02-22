0f3d89298b6a7ac793969f1504db84dab3293395
JiSeong Kim <122510664+JSK0406@users.noreply.github.com>
2023-02-22 10:50:40 +0900

    [Gold III] Title: 파티, Time: 820 ms, Memory: 34348 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -1,36 +1,30 @@
 import sys
 import heapq
 
-def func(start, end, edge):
-    shortest = [9999999]*(N+1)
-    visit = [0] * (N+1)
-    cur = start
+def dijkstra(start, end):
+    shortest = [999_999_999] * (N+1)
     shortest[start] = 0
-    hq = []
-    while sum(visit) < N and cur != end:
-        visit[cur] = 1
-        for next in range(1, N+1):
-            if edge[cur][next]!=0:
-                tmp = shortest[cur] + edge[cur][next]
-                if shortest[next] < tmp:
-                    continue
-                else:
-                    shortest[next] = shortest[cur] + edge[cur][next]
-                    if visit[next] == 0:
-                        heapq.heappush(hq, (shortest[next], next))
+    hq = [(shortest[start], start)]
+    while hq:
         cur = heapq.heappop(hq)[1]
+        if cur == end:
+            break
+        for i in edge[cur]:
+            tmp = shortest[cur] + i[0]
+            if shortest[i[1]] > tmp:
+                shortest[i[1]] = tmp
+                heapq.heappush(hq, (shortest[i[1]], i[1]))
     return shortest[end]
 
 N, M, X = map(int, sys.stdin.readline().split())
-edge = [[0 for _ in range(N+1)] for _ in range(N)]
-edge.insert(0, 0)
+edge = [[] for _ in range(N+1)]
 for _ in range(M):
     start, end, cost = map(int, sys.stdin.readline().split())
-    edge[start][end] = cost
+    edge[start].append((cost, end))
 
 result = 0
 for i in range(1, N+1):
     if i==X:
         continue
-    result = max(result, func(i, X, edge) + func(X, i, edge))
+    result = max(result, dijkstra(i, X) + dijkstra(X, i))
 print(result)
