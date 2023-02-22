6da6d76855058e188f77fc1706c96a2f4a590882
JiSeong Kim <122510664+JSK0406@users.noreply.github.com>
2023-02-22 13:21:31 +0900

    Update 녹색 옷 입은 애가 젤다지？.py

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
@@ -42,3 +42,31 @@ while True:
             cnt += 1
             break
 
+
+            
+# while True:
+#     N = int(input())
+#     if N == 0:
+#         break
+#     cost_lst = [0]
+#     for _ in range(N):
+#         cost_lst.append([0] + list(map(int, input().split())))
+#     lowest = [[INF for _ in range(N+1)] for _ in range(N+1)]
+#     lowest[1][1] = cost_lst[1][1]
+#     hq = [(cost_lst[1][1], 1, 1)]
+#     moveX = [1, -1, 0, 0]
+#     moveY = [0, 0, 1, -1]
+#     while hq:
+#         cost, row, col = heapq.heappop(hq)
+#         for i in range(4):
+#             next_Y = row+moveY[i]
+#             next_X = col+moveX[i]
+#             if (1 <= next_Y <= N) and (1 <= next_X <= N):
+#                 tmp = cost + cost_lst[next_Y][next_X]
+#                 if lowest[next_Y][next_X] > tmp:
+#                     lowest[next_Y][next_X] = tmp
+#                     heapq.heappush(hq, (tmp, next_Y, next_X))
+#         if lowest[N][N] != INF:
+#             print(f"Problem {cnt}: {lowest[N][N]}")
+#             cnt += 1
+#             break
