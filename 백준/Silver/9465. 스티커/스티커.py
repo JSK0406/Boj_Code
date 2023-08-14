2c7b3561887538ed32c34693e60ff5c30f601fa7
JiSeong Kim <122510664+JSK0406@users.noreply.github.com>
2023-08-14 15:12:07 +0900

    [Silver I] Title: 스티커, Time: 676 ms, Memory: 40712 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.py
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -5,12 +5,12 @@ for _ in range(int(input())):
     N = int(input())
     lst1 = list(map(int, input().split()))
     lst2 = list(map(int, input().split()))
-    dp1 = [lst1[0], lst1[0]]
-    dp2 = [lst2[0], lst2[0]]
+    dp10, dp11 = lst1[0], lst1[0]
+    dp20, dp21 = lst2[0], lst2[0]
 
     for i in range(1, N):
-        dp1[0] = dp1[1]
-        dp2[0] = dp2[1]
-        dp1[1] = max(dp2[0]+lst1[i], dp1[0])
-        dp2[1] = max(dp1[0]+lst2[i], dp2[0])
-    print(max(dp1[1], dp2[1]))
+        dp10 = dp11
+        dp20 = dp21
+        dp11 = max(dp20+lst1[i], dp10)
+        dp21 = max(dp10+lst2[i], dp20)
+    print(max(dp11, dp21))
