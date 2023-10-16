2fb2fad8b2d3b894b7ad16c08a7560b586939042
JiSeong Kim <122510664+JSK0406@users.noreply.github.com>
2023-10-17 08:11:48 +0900

    [Gold II] Title: 너 봄에는 캡사이신이 맛있단다, Time: 2544 ms, Memory: 66600 KB, Score: 250 point -BaekjoonHub

--- 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.py
+++ 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.py
@@ -5,9 +5,19 @@ N = int(input().strip())
 num_lst = list(map(int, input().split()))
 num_lst.sort()
 
+def num_pow(n):
+    if n == 0:
+        return 1
+    if n == 1:
+        return 2
+    if not n % 2:
+        return (num_pow(n//2) ** 2) % 1_000_000_007
+    else:
+        return (num_pow(n//2) ** 2 * 2) % 1_000_000_007
+
 ans = 0
 for i in range(N):
-    ans += (num_lst[i] * pow(2, i)) % 1000000007
-    ans -= (num_lst[i] * pow(2, N - 1 - i)) % 1000000007
+    ans += (num_lst[i] * num_pow(i)) % 1000000007
+    ans -= (num_lst[i] * num_pow(N - 1 - i)) % 1000000007
 
 print(ans % 1_000_000_007)
