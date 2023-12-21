d1d2daf127b42fe63c4cd36faa0c0f93e787e862
JiSeong Kim <122510664+JSK0406@users.noreply.github.com>
2023-12-22 00:07:22 +0900

    [Gold IV] Title: 최단경로, Time: 684 ms, Memory: 68392 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,40 +1,33 @@
 import sys
 input = sys.stdin.readline
 import heapq
-INF = 999_999_999
+INF = 999_999_999  # 문제에 따라 바뀔 수 있음
 
 V, E = map(int, input().split())  # 정점의 개수, 간선의 개수
-start_node = int(input())
-edge_dict = dict()  # 저장되는 데이터 [도착지점, 비용]
-lst = [INF for _ in range(V + 1)]
-lst[start_node] = 0
+start_num = int(input())  # 시작 점
+edge_lst = [[] for _ in range(V+1)]  # 노드가 1번부터 시작이기에 V+1로 처리
 
-for _ in range(E):  # 딕셔너리에 edge 추가
-    start, end, price = map(int, input().split())
-    try:
-        edge_dict[start].append((price, end))
-    except:
-        edge_dict[start] = [(price, end)]
+for _ in range(E):
+    start, end, price = map(int, input().split())  # from, to, price
+    edge_lst[start].append((end, price))
 
-hq = []
-heapq.heappush(hq, (0, start_node))
+def dijkstra(start_num):
+    dist_lst = [INF for _ in range(V+1)]  # 최단 거리를 보관하는 리스트
+    hq = []  # 우선 순위 큐 선언, (dist, node_num)의 원소들이 추가될 것
+    dist_lst[start_num] = 0  # 출발 노드는 dist = 0
+    heapq.heappush(hq, (0, start_num))  # 우선순위 큐에 출발 노드 추가
+    
+    while hq:
+        dist, now = heapq.heappop(hq)
+        
+        for next, price in edge_lst[now]:
+            if dist_lst[next] > dist + price:
+                dist_lst[next] = dist + price  # 동일한 경로의 최소 비용을 적용하기 위함
+                heapq.heappush(hq, (dist_lst[next], next))
+            
+    return dist_lst
 
-while hq:
-    distance, now_node = heapq.heappop(hq)
-    try:
-        now_edgelst = edge_dict[now_node]
-        if lst[now_node] < distance:
-            continue
-        for d, n in now_edgelst:
-            next_price = distance + d
-            if next_price < lst[n]:
-                lst[n] = next_price
-                heapq.heappush(hq, (next_price, n))
-    except:
-        continue
+dist_lst = dijkstra(start_num)
 
-for i in range(1, V + 1):
-    if lst[i] == INF:
-        print("INF")
-        continue
-    print(lst[i])
+for i in range(1, V+1):
+    print('INF' if dist_lst[i] == INF else dist_lst[i])
