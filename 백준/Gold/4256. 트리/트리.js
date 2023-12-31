07406e0c31d533f322310e3cd91a30e4c8ea6632
JiSeong Kim <122510664+JSK0406@users.noreply.github.com>
2023-12-31 14:22:47 +0900

    [Gold II] Title: 트리, Time: 272 ms, Memory: 16052 KB -BaekjoonHub

--- 백준/Gold/4256. 트리/트리.js
+++ 백준/Gold/4256. 트리/트리.js
@@ -4,21 +4,6 @@ let input = fs.readFileSync(filePath).toString().trim().split('\n');
 
 const T = Number(input.shift());
 
-const dfs = (preLeft, preRight, inLeft, inRight, preOrder, inOrder, inIdx) => {
-    if (preLeft > preRight || inLeft > inRight) {
-        return;
-    }
-    const nowRoot = preOrder[preLeft];
-    const rootIdx = inIdx[nowRoot];
-
-    const leftNode = rootIdx - inLeft - 1;  // 3
-    const rightNode = inRight - rootIdx;  // 3
-
-    dfs(preLeft+1, preLeft+leftNode+1, inLeft, inLeft+leftNode, preOrder, inOrder, inIdx);
-    dfs(preRight-rightNode+1, preRight, inRight-rightNode+1, inRight, preOrder, inOrder, inIdx);
-
-    process.stdout.write(`${nowRoot} `)
-}
 
 for (let _ = 0; _ < T; _++) {
     const N = Number(input.shift());
@@ -31,6 +16,24 @@ for (let _ = 0; _ < T; _++) {
         inIdx[inOrder[i]] = i;
     }
     
+    const ans = [];
+
+    const dfs = (preLeft, preRight, inLeft, inRight, preOrder, inOrder, inIdx) => {
+        if (preLeft > preRight || inLeft > inRight) {
+            return;
+        }
+        const nowRoot = preOrder[preLeft];
+        const rootIdx = inIdx[nowRoot];
+    
+        const leftNode = rootIdx - inLeft - 1;  // 3
+        const rightNode = inRight - rootIdx;  // 3
+    
+        dfs(preLeft+1, preLeft+leftNode+1, inLeft, inLeft+leftNode, preOrder, inOrder, inIdx);
+        dfs(preRight-rightNode+1, preRight, inRight-rightNode+1, inRight, preOrder, inOrder, inIdx);
+    
+        ans.push(nowRoot)
+    }
+    
     dfs(0, N-1, 0, N-1, preOrder, inOrder, inIdx);
-    console.log('')
+    console.log(ans.join(' '))
 }
